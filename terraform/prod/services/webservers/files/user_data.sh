24d410104167a64959218e2bcc9ae2a96a44069a
makschihin <maksymchihin@gmail.com>
2021-06-29 10:31:40 +0300

    Test

--- terraform/prod/services/webservers/files/user_data.sh
+++ terraform/prod/services/webservers/files/user_data.sh
@@ -3,7 +3,13 @@
   && sudo apt-add-repository --yes --update ppa:ansible/ansible \
   && sudo apt update -y \
   && sudo apt-get install git software-properties-common ansible -y \
-  && git clone https://github.com/Chmokachka/Dp-206-DevOps-Home.git /opt/home \
-  && ansible-playbook --connection=local  /opt/home/terraform-home/ansible/all_server.yml \
+  && sudo apt-get install openjdk-8-jdk -y \
+  && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y \
+  && wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add - \
+  && sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' \
+  && sudo apt-get update -y \
+  && sudo apt-get install jenkins -y \
+  && sudo service jenkins start \
+  && git clone https://github.com/makschihin/demo2_home.git /opt/demo2_home \
+  && ansible-playbook --connection=local  /opt/demo2_home/ansible/all_server.yml \
   && sudo echo "hello" >> /home/ubuntu/hello.txt
-  
